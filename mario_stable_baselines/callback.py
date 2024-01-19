c6b833cd77b5d17db59b45be36bb549cac5597c4
Dagmar Ottevangers <dagmar.ottevangers@vlaanderen.be>
2024-01-19 12:14:55 +0100

    added config to store with training run in callback and use for passing parameters

--- mario_stable_baselines/callback.py
+++ mario_stable_baselines/callback.py
@@ -1,17 +1,22 @@
+import json
 import os
 from stable_baselines3.common.callbacks import BaseCallback
 
 
 class TrainAndLoggingCallback(BaseCallback):
 
-    def __init__(self, check_freq, save_path, verbose=1):
+    def __init__(self, check_freq, save_path, config, verbose=1):
         super(TrainAndLoggingCallback, self).__init__(verbose)
         self.check_freq = check_freq
         self.save_path = save_path
+        # keep track of the configuration used for a training session
+        self.config = config
 
     def _init_callback(self):
         if self.save_path is not None:
             os.makedirs(self.save_path, exist_ok=True)
+            with open(self.save_path+"/config.json", "w") as outfile:
+                json.dump(self.config, outfile, indent=4)
 
     def _on_step(self):
         if self.n_calls % self.check_freq == 0:
