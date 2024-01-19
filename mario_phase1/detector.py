6f88ade9c32da9b123b577cedd68c8c45203ce12
maximvandecasteele <mvandecasteele@gmail.com>
2024-01-19 14:58:35 +0100

    detector output class names added (reads from yaml file)

--- mario_phase1/detector.py
+++ mario_phase1/detector.py
@@ -1,28 +1,31 @@
 import pandas as pd
+from typing import List
 from ultralytics import YOLO
+import yaml
 
 class Detector:
 
     def __init__(self):
         super().__init__()
-        self.model = YOLO('../mario_phase0/models/Mario_OD_vanilla_best.pt')
+        self.model = YOLO('models/Mario_OD_vanilla_best.pt')
+
+        # TODO location of final YAML file
+        with open('models/data.yaml','r') as file:
+            data = yaml.safe_load(file)
+            self.names = data['names']
 
     def detect(self, observation) -> pd.DataFrame:
-        # YOLO detection
-        # TODO change model to final one
-        #model = YOLO('../mario_phase0/models/Mario_OD_vanilla_best.pt')
 
         results = self.model(observation)
-
         # what if there are no detections?
         positions = pd.DataFrame(data=None, columns=['name', 'xmin', 'xmax', 'ymin', 'ymax'])
 
         for r in results:
-        #     TODO convert classes into names
             boxes = r.boxes.cpu().numpy()
             classes = pd.DataFrame(boxes.cls, columns=['class'])
             # other types of bounding box data can be chosen: xyxy, xywh, xyxyn, xywhn
             xywh = pd.DataFrame(boxes.xywh, columns=['x', 'y', 'w', 'h'])
-            positions = pd.concat([classes, xywh], axis=1)
+            names_pd = classes['class'].apply(lambda x: self.names[int(x)])
+            positions = pd.concat([classes, names_pd, xywh], axis=1)
 
         return positions
